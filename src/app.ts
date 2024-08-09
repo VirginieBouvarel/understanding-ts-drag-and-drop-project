b30bdbb3b7831e349924aa4937335a0b7738cd04
Virginie <vb@ikimo9.com>
2024-08-09 10:35:35 +0200

    ajout lodash

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+console.log(_.shuffle([1,2,3]));
+
 import ProjectInput from "./components/project-input";
 import ProjectList from "./components/project-list";
 import { ProjectStatus } from "./models/project";
